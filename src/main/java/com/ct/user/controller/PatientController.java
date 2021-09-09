6f369fa8f35d8b5cf8fff911be97a1083ac973e3
Yogendra Yadav <yogendrakumar.yadav@citiustech.com>
2021-09-09 17:40:23 +0530

    Roles Added

--- src/main/java/com/ct/user/controller/PatientController.java
+++ src/main/java/com/ct/user/controller/PatientController.java
@@ -3,20 +3,17 @@ package com.ct.user.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ct.user.model.Patient;
 import com.ct.user.service.PatientService;
-import com.sun.tools.sjavac.Log;
-
-import lombok.extern.log4j.Log4j;
 
 /**
  * Mapping for Patient
@@ -25,6 +22,7 @@ import lombok.extern.log4j.Log4j;
  *
  */
 @RestController
+@CrossOrigin(origins = "*")
 public class PatientController {
 
 	@Autowired
--- src/main/java/com/ct/user/controller/StaffController.java
+++ src/main/java/com/ct/user/controller/StaffController.java
@@ -3,49 +3,48 @@ package com.ct.user.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ct.user.model.Staff;
 import com.ct.user.service.StaffService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class StaffController {
 
 	@Autowired
 	private StaffService staffService;
-	
-	@GetMapping("/staffs")
-	public List<Staff> all(){
+
+	@GetMapping("/employees")
+	public List<Staff> all() {
 		return staffService.getAllStaffDetails();
 	}
-	
-	@PostMapping("/staffs")
+
+	@PostMapping("/employees")
 	public Staff newStaff(@RequestBody Staff staff) {
 		return staffService.save(staff);
 	}
-	
-	@GetMapping("/staffs/{id}")
+
+	@GetMapping("/employees/{id}")
 	public Staff one(@PathVariable long id) {
 		return staffService.getStaff(id);
 	}
-	
-	@PutMapping("/staffs/{id}")
+
+	@PutMapping("/employees/{id}")
 	public Staff replaceStaff(@PathVariable long id, @RequestBody Staff staff) {
 		return staffService.updateStaff(id, staff);
 	}
-	
-	@DeleteMapping("/staffs/{id}")
+
+	@DeleteMapping("/employees/{id}")
 	public void removeStaff(@PathVariable long id) {
 		staffService.disableStaff(id);
 	}
-	
 
-	
 }
