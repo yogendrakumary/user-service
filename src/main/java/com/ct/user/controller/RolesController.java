3c35052afe116bb85e148651ac965d22e94d386a
HarishB <harishb2@citiustech.com>
2021-10-27 21:55:14 +0530

    added schedule

--- src/main/java/com/ct/user/controller/RolesController.java
+++ src/main/java/com/ct/user/controller/RolesController.java
@@ -1,10 +1,12 @@
 package com.ct.user.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ct.user.model.Role;
@@ -21,5 +23,9 @@ public class RolesController {
 	@GetMapping("/roles")
 	public List<Role> all() {
 		return rolesService.getAllRoles();
+	}
+	@GetMapping("/roles/{id}")
+	public Optional<Role> getRole(@PathVariable int id) {
+		return rolesService.getRole(id);
 	}
 }
